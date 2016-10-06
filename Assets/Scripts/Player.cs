1c623bc417c25edfeab890b99fb80381b6a5c751
mplawley <mark.lawley@gmail.com>
2016-10-06 16:58:46 -0400

    Card player AI foundation built

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//The player can be either human or an AI
+public enum PlayerType
+{
+	human,
+	ai
+}
+
+//The individual player of the game. Note: Player does NOT extend MonoBehaviour or any other class
+
+[System.Serializable] //Make Player class visible in inspector
+public class Player
+{
+	public PlayerType type = PlayerType.ai;
+	public int playerNum;
+
+	public List<CardBartok> hand; //The cards in this player's hand
+	public SlotDef handSlotDef;
+
+	//Add a card to the hand
+	public CardBartok AddCard(CardBartok eCB)
+	{
+		if (hand == null)
+		{
+			hand = new List<CardBartok>();
+		}
+
+		//Add the card to the hand
+		hand.Add(eCB);
+
+		return eCB;
+	}
+
+	//Remove a card from the hand
+	public CardBartok RemoveCard(CardBartok cb)
+	{
+		hand.Remove(cb);
+		return cb;
+	}
+}
